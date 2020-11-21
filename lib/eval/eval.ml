70455e5da20a6a9b15ba69c05fa80c86d653e51f
Isaac-DeFrain <isaacdefrain@gmail.com>
2020-11-21 11:49:00 -0700

    Improve type checking in eval and reduce
    Add reduce_full

--- lib/eval/eval.ml
+++ lib/eval/eval.ml
@@ -14,10 +14,13 @@ let rec eval env = function
 | App (Lam (x, t, body), g) ->
     let pseudo = TermMap.singleton (Var x) t in
     let g_t = type_assignment pseudo g in
-    if t = g_t then
-      let env' = VarMap.add x (eval env g) env in
-      eval env' body
-    else Villtyped ("The type of " ^ string_of_term g ^ " does not match " ^ string_of_type t)
+    let env' = VarMap.add x (eval env g) env in
+    begin match t with
+    | TVar _ -> eval env' body
+    | _ ->
+        if t = g_t then eval env' body
+        else Villtyped ("The type of " ^ string_of_term g ^ " does not match " ^ string_of_type t)
+    end
 | App (_, _) as app ->
     let app' = reduce app in
     if app' = app then Virreducible
@@ -33,10 +36,38 @@ and reduce = function
 | Var _ as v -> v
 | Lam (x, t, body) -> Lam (x, t, reduce body)
 | App (Lam (x, t, body), g) as self ->
-    if t = type_assignment (TermMap.singleton (Var x) t) g
-    then sub g x body
-    else self
+    let pseudo = TermMap.singleton (Var x) t in
+    begin match t with
+    | TVar _ -> sub g x body
+    | _ ->
+        if t = type_assignment pseudo g
+        then sub g x body
+        else self
+    end
 | App (f, g) -> App (reduce f, reduce g)
+| Bunop (unop, b) -> BLit (unop b)
+| Bbiop (biop, a, b) -> BLit (biop a b)
+| Iunop (unop, i) -> ILit (unop i)
+| Ibiop (biop, i, j) -> ILit (biop i j)
+| lit -> lit
+
+and reduce_full = function
+| Var _ as v -> v
+| Lam (x, t, body) -> Lam (x, t, reduce_full body)
+| App (Lam (x, t, body), g) as self ->
+    let pseudo = TermMap.singleton (Var x) t in
+    begin match t with
+    | TVar _ -> reduce_full (sub g x body)
+    | _ ->
+        if t = type_assignment pseudo g
+        then reduce_full (sub g x body)
+        else self
+    end
+| App (f, g) ->
+    begin match reduce f with
+    | Lam (_, _, _) as lam -> reduce_full (App (lam, g))
+    | _ -> App (reduce_full f, reduce_full g)
+    end
 | Bunop (unop, b) -> BLit (unop b)
 | Bbiop (biop, a, b) -> BLit (biop a b)
 | Iunop (unop, i) -> ILit (unop i)
