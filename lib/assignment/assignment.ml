e8d05bbe84e7e698aa0864495db973442a566eed
Isaac-DeFrain <isaacdefrain@gmail.com>
2020-11-20 18:55:38 -0700

    Add variable to function types
    Print types

--- lib/assignment/assignment.ml
+++ lib/assignment/assignment.ml
@@ -10,16 +10,22 @@ module TermMap = Map.Make(Term)
 type pseudocontext = t TermMap.t
 
 let rec type_vars = function
-| TFun (t_var, t_body) -> type_vars t_var @ type_vars t_body
-| TPi (t_var, t_body) -> type_vars t_var @ type_vars t_body
+| TFun (x, t_var, t_body) -> x :: type_vars t_var @ type_vars t_body
+| TPi (x, t_var, t_body) -> x :: type_vars t_var @ type_vars t_body
 | _ -> []
 
 let rec type_assignment pseudo = function
 | BLit _ -> TBool
+| Bunop (_, _) -> TBool
+| Bbiop (_, _, _) -> TBool
 | ILit _ -> TInt
+| Iunop (_, _) -> TInt
+| Ibiop (_, _, _) -> TInt
 | Var _ as v -> TermMap.find v pseudo
 | Lam (x, t, body) ->
     let pseudo' = TermMap.add (Var x) t pseudo in
     let t_body = type_assignment pseudo' body in
-    TPi (t, t_body)
+    let t_vars = type_vars t_body in
+    if List.mem x t_vars then TPi (x, t, t_body)
+    else TFun (x, t, t_body)
 | _ -> TStar
--- lib/type/type.ml
+++ lib/type/type.ml
@@ -23,10 +23,19 @@ and env = value VarMap.t
 and t =
   | TBool
   | TInt
-  | TFun of t * t
-  | TPi of t * t
+  | TFun of string * t * t
+  | TPi of string * t * t
   | TStar
 
 and kind =
   | Base of t
   | Higher of t
+
+let rec to_string = function
+| TBool -> "Bool"
+| TInt -> "Int"
+| TFun (_, t1, t2) -> to_string t1 ^ " -> " ^ to_string t2
+| TPi (x, t1, t2) -> "Pi " ^ x ^ ": " ^ to_string t1 ^ ". " ^ to_string t2
+| TStar -> "*"
+
+let print t = to_string t |> print_endline
