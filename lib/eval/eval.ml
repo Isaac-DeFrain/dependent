098ca49eeb51cd8009989b732a94f84d3286df87
Isaac-DeFrain <isaacdefrain@gmail.com>
2020-11-22 17:01:45 -0700

    Add type env to eval, reduce, and reduce_full

--- lib/eval/eval.ml
+++ lib/eval/eval.ml
@@ -1,30 +1,35 @@
 open Assignment
 open Type
 
-let rec sub e1 x e2 =
-  match e2 with
-  | Var v as y-> if v = x then e1 else y
-  | Lam (v, t, body) as lam -> if v = x then lam else Lam (v, t, sub e1 x body)
-  | App (f, g) -> App (sub e1 x f, sub e1 x g)
-  | x -> x
+let rec sub e x = function
+  | Var v as y-> if v = x then e else y
+  | Lam (v, t, body) as lam -> if v = x then lam else Lam (v, t, sub e x body)
+  | App (f, g) -> App (sub e x f, sub e x g)
+  | atomic -> atomic
 
-let rec eval env = function
-| Var x -> VarMap.find x env
-| Lam (_, _, _) as lam -> Vclosure (reduce lam, env)
+let rec eval venv tenv = function
+| Var x -> VarMap.find x venv
+| Lam (_, _, _) as lam -> Vclosure (reduce tenv lam, venv)
 | App (Lam (x, t, body), g) ->
     let pseudo = TermMap.singleton (Var x) t in
-    let g_t = type_assignment pseudo g in
-    let env' = VarMap.add x (eval env g) env in
+    let g_t = type_assignment tenv pseudo g in
+    let venv' = VarMap.add x (eval venv tenv g) venv in
     begin match t with
-    | TVar _ -> eval env' body
+    | TVar v ->
+        begin match VarMap.find_opt v tenv with
+        | Some v_t ->
+            if v_t = g_t then eval venv' tenv body
+            else illtyped g v_t
+        | None -> eval venv' tenv body
+        end
     | _ ->
-        if t = g_t then eval env' body
-        else Villtyped ("The type of " ^ string_of_term g ^ " does not match " ^ string_of_type t)
+        if t = g_t then eval venv' tenv body
+        else illtyped g t
     end
 | App (_, _) as app ->
-    let app' = reduce app in
-    if app' = app then Virreducible
-    else eval env app'
+    let app' = reduce tenv app in
+    if app' = app then Virreducible app
+    else eval venv tenv app'
 | BLit b -> Vbool b
 | Bunop (unop, b) -> Vbool (unop b)
 | Bbiop (biop, a, b) -> Vbool (biop a b)
@@ -32,44 +37,55 @@ let rec eval env = function
 | Iunop (unop, i) -> Vint (unop i)
 | Ibiop (biop, i, j) -> Vint (biop i j)
 
-and reduce = function
+and reduce tenv = function
 | Var _ as v -> v
-| Lam (x, t, body) -> Lam (x, t, reduce body)
+| Lam (x, t, body) -> Lam (x, t, reduce tenv body)
 | App (Lam (x, t, body), g) as self ->
     let pseudo = TermMap.singleton (Var x) t in
     begin match t with
     | TVar _ -> sub g x body
     | _ ->
-        if t = type_assignment pseudo g
+        if t = type_assignment tenv pseudo g
         then sub g x body
         else self
     end
-| App (f, g) -> App (reduce f, reduce g)
+| App (f, g) -> App (reduce tenv f, reduce tenv g)
 | Bunop (unop, b) -> BLit (unop b)
 | Bbiop (biop, a, b) -> BLit (biop a b)
 | Iunop (unop, i) -> ILit (unop i)
 | Ibiop (biop, i, j) -> ILit (biop i j)
 | lit -> lit
 
-and reduce_full = function
+and reduce_full tenv = function
 | Var _ as v -> v
-| Lam (x, t, body) -> Lam (x, t, reduce_full body)
+| Lam (x, t, body) -> Lam (x, t, reduce_full tenv body)
 | App (Lam (x, t, body), g) as self ->
     let pseudo = TermMap.singleton (Var x) t in
     begin match t with
-    | TVar _ -> reduce_full (sub g x body)
+    | TVar _ -> reduce_full tenv (sub g x body)
     | _ ->
-        if t = type_assignment pseudo g
-        then reduce_full (sub g x body)
+        if t = type_assignment tenv pseudo g
+        then reduce_full tenv (sub g x body)
         else self
     end
 | App (f, g) ->
-    begin match reduce f with
-    | Lam (_, _, _) as lam -> reduce_full (App (lam, g))
-    | _ -> App (reduce_full f, reduce_full g)
+    begin match reduce tenv f with
+    | Lam (_, _, _) as lam -> reduce_full tenv (App (lam, g))
+    | _ -> App (reduce_full tenv f, reduce_full tenv g)
     end
 | Bunop (unop, b) -> BLit (unop b)
 | Bbiop (biop, a, b) -> BLit (biop a b)
 | Iunop (unop, i) -> ILit (unop i)
 | Ibiop (biop, i, j) -> ILit (biop i j)
 | lit -> lit
+
+and illtyped term t = Villtyped
+  (String.concat " "
+  [ "The type of"
+  ; string_of_term term
+  ; " does not match"
+  ; string_of_type t])
+
+let eval' = eval VarMap.empty VarMap.empty
+let reduce' = reduce VarMap.empty
+let reduce_full' = reduce_full VarMap.empty
