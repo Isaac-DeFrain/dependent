1faa46acba27b6a475753e3f69221f8c585aa18e
Isaac-DeFrain <isaacdefrain@gmail.com>
2020-11-30 19:48:56 -0700

    Improve display of test results

--- test/utils.ml
+++ test/utils.ml
@@ -6,8 +6,11 @@ let print_res name res =
     Printf.printf "\027[38;5;2m%s\027[0m" (name ^ " test" ^ " PASSED\n")
   else Printf.printf "\027[38;5;1m%s\027[0m" (name ^ " test" ^ " FAILED\n")
 
-let print_all () =
-  if !all_tests then Printf.printf "\n\027[38;5;2m%s\027[0m" "ALL TESTS PASSED"
-  else Printf.printf "\n\027[38;5;1m%s\027[0m" "SOME TEST(S) FAILED"
+let print_all name =
+  let name' = String.uppercase_ascii name in
+  if !all_tests then
+    Printf.printf "\n\027[38;5;2m%s\027[0m" ("ALL " ^ name' ^ " TESTS PASSED")
+  else
+    Printf.printf "\n\027[38;5;1m%s\027[0m" ("SOME " ^ name' ^ " TEST(S) FAILED")
 
 let test_group ~name ~desc = Printf.printf "*%s*\n  %s\n" name desc
